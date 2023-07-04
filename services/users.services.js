506783920b1b661313f843e3ffd9f4ff976a265d
Idan Asayag <idanasayag0@gmail.com>
2023-07-04 10:16:19 +0300

    create user + wishlist service.

---
+++ services/users.services.js
@@ -0,0 +1,45 @@
+const User = require("../models/userSchema");
+
+const getUserByEmail = async (email) => {
+  const user = await User.findOne({ email: email }).populate("orders");
+  return user;
+};
+
+const getUserById = async (id) => {
+  return await User.findById(id);
+};
+
+const getAll = async () => {
+  try {
+    const users = await User.find();
+    if (users) {
+      return users;
+    }
+  } catch (err) {}
+};
+
+const updateUser = async (id, user) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, user);
+    if (updatedUser) {
+      return updatedUser;
+    }
+  } catch (error) {}
+};
+
+const deleteUser = async (id) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (deletedUser) {
+      return deletedUser;
+    }
+  } catch (error) {}
+};
+
+module.exports = {
+  getUserByEmail,
+  getUserById,
+  updateUser,
+  getAll,
+  deleteUser,
+};
